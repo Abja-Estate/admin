7455ddea0b35e5e65963489f1c85252fb27eec67
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2025-07-03 14:37:05 +0100

    .

--- config/index.ts
+++ config/index.ts
@@ -1,1 +1,1 @@
-export const BASE_URL: string ="http://localhost:3001/api" //"https://api.abjapropertymanagement.com/api";
+export const BASE_URL: string ="https://api.abjapropertymanagement.com/api";
